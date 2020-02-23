f43dc5b117fe9d25cafa1ba97be1e89eadb3086d
Abdullah272056 <abdullah272056@gmail.com>
2020-02-23 15:22:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/pdf_view/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pdf_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Pdf.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/pdf_view/Pdf.java
@@ -0,0 +1,32 @@
+package com.example.pdf_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+
+public class Pdf extends AppCompatActivity {
+    PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+        pdfView=findViewById(R.id.pdfViewId);
+        pdfView.fromAsset("computer.pdf").enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(true)
+                .pages(0,1,2,3,4)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .load();
+    }
+}
